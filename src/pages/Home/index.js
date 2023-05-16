0fdee24661c21dd8118384eefc9928852197a667
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-16 08:54:57 -0300

    instala servidor, configura api e faz a requisicao

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import api from '../../assets/services/api';
 
 export default function Home() {
+  const [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    async function loadApi() {
+      const response = await api.get('trips');
+      setTrips(response.data);
+      console.log(response.data);
+    }
+
+    loadApi();
+  }, []);
   return <div>Home</div>;
 }
