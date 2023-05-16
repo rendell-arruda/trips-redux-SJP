03b8c40ece9ba21e84b2d40755596152d4ce44ee
Rendell Arruda <rendellarruda3@gmail.com>
2023-05-16 09:44:19 -0300

    estiliza reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import './style.css';
+import { MdDelete } from 'react-icons/md';
 
 export default function Reservas() {
-  return <div>Reservas</div>;
+  return (
+    <div>
+      <h1 className="title">Você solicitou 1 reservas</h1>
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="Maceió"
+        />
+        <strong>Viagem Maceió 7 dias</strong>
+        <span>Quantidade: 2</span>
+        <button type="button" onClick={() => {}}>
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+      <footer>
+        <button type="button">Solicitar reservas</button>
+      </footer>
+    </div>
+  );
 }
